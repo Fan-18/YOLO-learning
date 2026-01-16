7819a61fe5da3e3442ca932092e77501d985b8fa
Jack_11 <1046263905@qq.com>
2026-01-16 19:04:17 +0800

    update gitignore and refresh cache

--- YOLO13/yolov13-main/ultralytics/cfg/models/11/test.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model = YOLO("/home/jack/11/1027/YOLO13/yolov13-main/ultralytics/cfg/models/11/yolo11-p2.yaml")
-print(model.info()) # 检查是否有 4 个 Detection Layers
-print(f"Model strides: {model.model.stride}")
